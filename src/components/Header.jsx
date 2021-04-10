cfd14aa90d0eaf550ead75fdec35dc9eb3a78d18
nimos <nimos.cmp@gmail.com>
2021-04-10 18:06:43 -0500

    Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = ({title}) => {
+    return (  
+        <>
+            <header>
+                <nav>
+                    <h1>{title}</h1>
+                </nav>
+            </header>
+        </>
+    );
+}
+ 
+export default Header;
