1eda73ff27683b8eac12f82204fb1053a1fa3f94
nimos <nimos.cmp@gmail.com>
2021-04-10 16:50:24 -0500

    Create basic components

---
+++ src/components/SetPlace.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import Button from '@material-ui/core/Button';
+
+const SetPlace = () => {
+    return (  
+        <>
+        </>
+    );
+}
+ 
+export default SetPlace;
