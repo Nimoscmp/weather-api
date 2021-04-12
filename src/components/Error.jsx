b67e14e54e618bc3baa8b3b6ec348d3477c08452
nimos <nimos.cmp@gmail.com>
2021-04-12 03:56:53 -0500

    Create error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+const Error = () => (  
+    <>
+        <aside className="col-11 col-md-5 p-2">
+            <div className="m-0 p-0 row rounded border" id="card">
+                <p className="text-secondary mt-2">No se encontraron resultados</p>
+                <p>Intenta de nuevo, con una ciudad correspondiente al país</p>
+            </div>
+        </aside>
+    </>
+);
+
+export default Error;
